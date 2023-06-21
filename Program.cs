a07b4e301239630b4b9420e1aa6f09cb66437a23
mfernandes <mfernandes@tecnologia-unica.com.br>
2023-06-21 19:25:41 -0300

    subindo projeto

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿string input = "a quick brown fox jumps over the lazy dog";
+bool containsAllLetters = HasAllLetters(input);
+
+if (containsAllLetters)
+    Console.WriteLine("A string contém todas as letras do alfabeto.");
+else
+    Console.WriteLine("A string não contém todas as letras do alfabeto.");
+
+static bool HasAllLetters(string input)
+{
+    bool[] alphabetFlags = new bool[26];
+    int count = 0;
+
+    foreach (char c in input.ToLower())
+    {
+        if (char.IsLetter(c))
+        {
+            int index = c - 'a';
+            if (!alphabetFlags[index])
+            {
+                alphabetFlags[index] = true;
+                count++;
+            }
+        }
+    }
+
+    return count == 26;
+}
